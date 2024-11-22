7e4d441a1c7d569004d9ee24c1cf5c640bb6b054
Christian Duque <cduque10@wgu.edu>
2024-11-21 18:48:36 -0800

    Initial Commit.

---
+++ src/main/java/com/d288/cduque10/Cduque10Application.java
@@ -0,0 +1,13 @@
+package com.d288.cduque10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cduque10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cduque10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/d288/cduque10/Cduque10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.cduque10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cduque10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
